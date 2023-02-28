d94bade0cd0f08aee50057eb3e9ce2051de57c36
den-m1985 <denismartyanov244@gmail.com>
2023-02-28 19:47:44 +0200

    Seminar 6

---
+++ Seminar02/terminal/Command.java
@@ -0,0 +1,21 @@
+package terminal;
+
+
+import java.util.List;
+
+public class Command implements CommandParser {
+    private List<String> inputCommand;
+
+
+    public Command(List<String> inputCommand) {
+        this.inputCommand = inputCommand;
+    }
+
+
+    @Override
+    public String[] parseCommand(String inputCommand) {
+        return new String[0];
+    }
+
+
+}
